ba24170deaf07d77fd07ea4a492a36f3d4810bed
Quenton Barger <qbarger22@gmail.com>
2025-04-01 20:23:04 -0600

    Added load test script

---
+++ loadTests/loginAndOrder.js
@@ -0,0 +1,199 @@
+import { sleep, check, group, fail } from "k6";
+import http from "k6/http";
+import jsonpath from "https://jslib.k6.io/jsonpath/1.0.2/index.js";
+
+export const options = {
+  cloud: {
+    distribution: {
+      "amazon:us:ashburn": { loadZone: "amazon:us:ashburn", percent: 100 },
+    },
+    apm: [],
+  },
+  thresholds: {},
+  scenarios: {
+    Scenario_1: {
+      executor: "ramping-vus",
+      gracefulStop: "30s",
+      stages: [
+        { target: 5, duration: "30s" },
+        { target: 15, duration: "1m" },
+        { target: 10, duration: "30s" },
+        { target: 0, duration: "30s" },
+      ],
+      gracefulRampDown: "30s",
+      exec: "scenario_1",
+    },
+  },
+};
+
+export function scenario_1() {
+  let response;
+
+  const vars = {};
+
+  group("Login and Order - https://pizza.sobolo.click/", function () {
+    // Homepage
+    response = http.get("https://pizza.sobolo.click/", {
+      headers: {
+        accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "max-age=0",
+        "if-modified-since": "Wed, 12 Mar 2025 02:36:57 GMT",
+        "if-none-match": '"6f0d724bedd219ca337acdd578e50dcb"',
+        priority: "u=0, i",
+        "sec-ch-ua":
+          '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"macOS"',
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+      },
+    });
+    sleep(26.3);
+
+    // Login
+    response = http.put(
+      "https://pizza-service.sobolo.click/api/auth",
+      '{"email":"qt@jwt.com","password":"qt"}',
+      {
+        headers: {
+          accept: "*/*",
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          "content-type": "application/json",
+          origin: "https://pizza.sobolo.click",
+          priority: "u=1, i",
+          "sec-ch-ua":
+            '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"macOS"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+        },
+      }
+    );
+    if (
+      !check(response, {
+        "status equals 200": (response) => response.status.toString() === "200",
+      })
+    ) {
+      console.log(response.body);
+      fail("Login was *not* 200");
+    }
+
+    vars["token1"] = jsonpath.query(response.json(), "$.token")[0];
+
+    sleep(5.6);
+
+    // Menu
+    response = http.get("https://pizza-service.sobolo.click/api/order/menu", {
+      headers: {
+        accept: "*/*",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "en-US,en;q=0.9",
+        authorization: `Bearer ${vars["token1"]}`,
+        "content-type": "application/json",
+        origin: "https://pizza.sobolo.click",
+        priority: "u=1, i",
+        "sec-ch-ua":
+          '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"macOS"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+      },
+    });
+
+    // Get franchise
+    response = http.get("https://pizza-service.sobolo.click/api/franchise", {
+      headers: {
+        accept: "*/*",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "en-US,en;q=0.9",
+        authorization: `Bearer ${vars["token1"]}`,
+        "content-type": "application/json",
+        origin: "https://pizza.sobolo.click",
+        priority: "u=1, i",
+        "sec-ch-ua":
+          '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"macOS"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+      },
+    });
+    sleep(6.2);
+
+    // Order
+    response = http.post(
+      "https://pizza-service.sobolo.click/api/order",
+      '{"items":[{"menuId":1,"description":"Veggie","price":0.0038}],"storeId":"1","franchiseId":1}',
+      {
+        headers: {
+          accept: "*/*",
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          authorization: `Bearer ${vars["token1"]}`,
+          "content-type": "application/json",
+          origin: "https://pizza.sobolo.click",
+          priority: "u=1, i",
+          "sec-ch-ua":
+            '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"macOS"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+        },
+      }
+    );
+    if (
+      !check(response, {
+        "status equals 200": (response) => response.status.toString() === "200",
+      })
+    ) {
+      console.log(response.body);
+      fail("Order was *not* 200");
+    }
+
+    vars.jwt = response.json().jwt;
+    if (!vars.jwt) {
+      fail("JWT token was not set properly.");
+    }
+
+    sleep(2.1);
+
+    // Verify order
+    response = http.post(
+      "https://pizza-factory.cs329.click/api/order/verify",
+      JSON.stringify({ jwt: vars.jwt }),
+      {
+        headers: {
+          accept: "*/*",
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          authorization: `Bearer ${vars["token1"]}`,
+          "content-type": "application/json",
+          origin: "https://pizza.sobolo.click",
+          priority: "u=1, i",
+          "sec-ch-ua":
+            '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"macOS"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "cross-site",
+          "sec-fetch-storage-access": "none",
+        },
+      }
+    );
+  });
+}
